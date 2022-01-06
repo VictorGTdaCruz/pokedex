f0ce9577d6fa9fac2215950b77486c9e88240098
Victor Cruz <victorgtdacruz@outlook.com>
2022-01-06 12:56:51 -0300

    Add gson, kodein android and ktx serialization dependencies and internet permission

--- networking/src/main/java/com/victor/networking/di/networkingModule.kt
+++ networking/src/main/java/com/victor/networking/di/networkingModule.kt
@@ -5,12 +5,14 @@ import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.singleton
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkingModule = DI.Module("networking") {
 
     bind<Retrofit>() with singleton {
         Retrofit.Builder()
             .baseUrl(Constants.pokeApiBaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 }
