54fea28fdb36f8f33229bbed5357afbff677a503
Victor Cruz <victorgtdacruz@outlook.com>
2022-01-08 17:02:42 -0300

    Add loading behavior to pokemon type list screen

--- pokedex/src/main/java/com/victor/pokedex/presentation/PokedexActivity.kt
+++ pokedex/src/main/java/com/victor/pokedex/presentation/PokedexActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import com.victor.pokedex.di.viewModel
-import com.victor.pokedex.presentation.ui.TypeList
+import com.victor.pokedex.presentation.ui.TypeListScreen
 import com.victor.pokedex.presentation.ui.theme.PokedexTheme
 import org.kodein.di.DIAware
 import org.kodein.di.android.closestDI
@@ -17,7 +13,6 @@ import org.kodein.di.android.closestDI
 class PokedexActivity : ComponentActivity(), DIAware {
 
     override val di by closestDI()
-
     private val viewModel: PokedexViewModel by viewModel()
 
     @ExperimentalFoundationApi
@@ -28,13 +23,11 @@ class PokedexActivity : ComponentActivity(), DIAware {
 
         setContent {
             PokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Column {
-                        Text(text = "Choose your type!")
-                        TypeList(viewModel.pokemonTypes) { }
-                    }
-                }
+                TypeListScreen(
+                    types = viewModel.pokemonTypes,
+                    onTypeClick = {  },
+                    isLoading = viewModel.isLoading.value
+                )
             }
         }
     }
--- pokedex/src/main/java/com/victor/pokedex/presentation/PokedexViewModel.kt
+++ pokedex/src/main/java/com/victor/pokedex/presentation/PokedexViewModel.kt
@@ -1,10 +1,12 @@
 package com.victor.pokedex.presentation
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.victor.pokedex.domain.model.PokemonTypeSimplified
 import com.victor.pokedex.domain.service.PokedexService
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 internal class PokedexViewModel(
@@ -14,13 +16,17 @@ internal class PokedexViewModel(
     var pokemonTypes = mutableStateListOf<PokemonTypeSimplified>()
         private set
 
-    fun loadPokemonTypes() {
-        viewModelScope.runCatching {
-            launch {
+    val isLoading = mutableStateOf(false)
 
-                val list = infrastructure.getPokemonTypes()
-                pokemonTypes.addAll(list)
-            }
+    fun loadPokemonTypes() {
+        viewModelScope.launch {
+            isLoading.value = true
+
+            delay(2000)
+            val list = infrastructure.getPokemonTypes()
+            pokemonTypes.addAll(list)
+
+            isLoading.value = false
         }
     }
 }
